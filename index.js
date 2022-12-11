109bc873223b563aa5567bc28e278b2f2a02de6c
Henrique Moraes <ahmdesouza@gmail.com>
2022-12-10 21:16:31 -0300

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+function detPopup()
+{alert("Defina qual a distância terão os postes entre sí.");}
+
+function altpPopup()
+{alert("Defina qual altura do poste a ser instalado.");}
+
+function cabdPopup()
+{alert("Informe qual tipo de fiação deseja utilizar.");}
+
+function linpPopup()
+{alert("Esta é a fiação disponível para instalação da linha primária, a qual ficará ostentada no topo do poste.");}
+
+function linsPopup()
+{alert("Esta é a fiação disponível para instalação da linha secundária, a qual será fixada na parte intermediária do poste.");}
+
+function lintPopup()
+{alert("Marque a caixa caso a instalação tenha algum outro tipo de fiação, como cabos de telefone, tv a cabo, etc.");}
+
+function vbrPopup()
+{alert("Informe a região de instalação para ter ciência quanto a velocidade base do vento a ser considerada.");}
+
+function vblPopup()
+{alert("Informe o local para ter ciência quanto a média variada de vento ocorrida.");}
+
