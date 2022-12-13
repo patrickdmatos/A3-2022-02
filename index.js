4153900dbf3e5792bf62037c48256abcc8c8eddd
patrickdmatos <patrick,1101reis@gmail.com>
2022-12-13 10:59:07 -0300

    concluido 1.0

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ let selectRN = document.getElementById('select-rn');
 let btnCalcular = document.getElementById('btncalculo');
 let selectFP = document.getElementById('select-vao');
 let selectFS = document.getElementById('campo-seletor-fs');
-
+let selectVB = document.getElementById('select-cabos-dispo');
+let RES1 = document.getElementById('RES01');
+let RES2 = document.getElementById('RES02');
+let RES3 = document.getElementById('RES03');
+let RES4 = document.getElementById('RES04');
 
 /* Desativar o select caso selecioe o 80 ou 120 */
-selectCabo.innerHTML = 
-`
+selectCabo.innerHTML =
+    `
 <select class="input_valor" id="dispost">
     <option selected>Distância</option>
     <option value="35">35 metros</optiojn>
@@ -37,16 +41,16 @@ selectCabo.innerHTML =
 selectCabo.onchange = () => {
     if (selectCabo.value == '35') {
         selectFP.innerHTML =
-        `
+            `
                                 <select class="input_valor" id="select-vao">
                                     <option selected>Cabeamento</option>
                                     <option value="187">35mm²</option>
                                     <option value="373">70mm²</option>
                                     <option value="659">185mm²</option>
                                 </select>
-        `;    
+        `;
         selectFS.innerHTML =
-        `
+            `
         <b>LINHA SECUNDÁRIA
         <br>FS / CABOS DISPONÍVEIS
     
@@ -55,18 +59,18 @@ selectCabo.onchange = () => {
         </br>
 
         
-        <select class="input_valor" id="select-vao">
-            <option selected>Cabeamento</option>
+        <select class="input_valor" id="campo-seletor-fs">
+            <option>Cabeamento</option>
             <option value="187">35mm²</option>
             <option value="373">70mm²</option>
             <option value="659">185mm²</option>
         </select>
     </b>
-        `;    
+        `;
     }
     if (selectCabo.value == '80') {
-        selectCabo.innerHTML = 
-        `
+        selectCabo.innerHTML =
+            `
         <select class="input_valor" id="dispost">
             <option >Distância</option>
             <option value="35">35 metros</optiojn>
@@ -74,33 +78,33 @@ selectCabo.onchange = () => {
             <option disabled>120 metros</option> <!--TRABALHA SOMENTE COM FORÇA PRIMÁRIA-->
         </select>'
         `;
-        
+
         selectFP.innerHTML =
-        `
+            `
                                 <select class="input_valor" id="select-vao">
                                     <option selected>Cabeamento</option>
                                     <option value="448">35mm²</option>
                                     <option value="549">70mm²</option>
                                     <option value="837">185mm²</option>
                                 </select>
-        `;                
+        `;
     }
     if (selectCabo.value == '120') {
-        selectCabo.innerHTML = 
-        `
+        selectCabo.innerHTML =
+            `
         <select class="input_valor" id="dispost">
             <option >Distância</option>
             <option value="35">35 metros</optiojn>
             <option disabled>80 metros</option> <!--TRABALHA SOMENTE COM FORÇA PRIMÁRIA-->
             <option selected value="120">120 metros</option> <!--TRABALHA SOMENTE COM FORÇA PRIMÁRIA-->
         </select>'
-        `        
+        `
     }
 };
 selectAlturaPoste.onchange = () => {
-if (selectAlturaPoste.value == '9') {
-    selectRN.innerHTML = 
-    `
+    if (selectAlturaPoste.value == '9') {
+        selectRN.innerHTML =
+            `
     <select class="input_valor" id="select-rn">
         <option selected>RN(DaN)</option>
         <option value="200">200</option>
@@ -108,10 +112,10 @@ if (selectAlturaPoste.value == '9') {
         <option value="600">600</option>
     </select>
     `
-}
-if (selectAlturaPoste.value == '12') {
-    selectRN.innerHTML = 
-    `
+    }
+    if (selectAlturaPoste.value == '12') {
+        selectRN.innerHTML =
+            `
     <select class="input_valor" id="select-rn">
         <option selected>RN(DaN)</option>
         <option value="400">400</option>
@@ -119,8 +123,11 @@ if (selectAlturaPoste.value == '12') {
         <option value="1000">1000</option>
     </select>
     `
-}
+    }
 };
+selectVB.onchange = () => {
+    calcular();
+}
 
 
 //INICIO DA FORMULA DO CALCULO
@@ -128,11 +135,11 @@ if (selectAlturaPoste.value == '12') {
 const E = '42501010000';
 const LF = selectAlturaPoste * 2;
 const MB = selectAlturaPoste * selectRN;
-const VB = 6.558675;
+const VB = ((25 * 25) * 0.00471) * 2.228;
 const Fmax = 0;
-if (selectFS.value) {
-    Fmax = selectFP.value + selectFS.value + VB;    
-}else{
+if (selectFS.value !== undefined) {
+    Fmax = selectFP.value + selectFS.value + VB;
+} else {
     Fmax = selectFP.value + VB;
 }
 const WA = 0;
@@ -150,28 +157,53 @@ if (selectRN == '1000') {
     WA = 859;
 };
 
-if(WA == 223 && selectAlturaPoste == '9'){
-    WB = 1583	
+if (WA == 223 && selectAlturaPoste == '9') {
+    WB = 1583
 };
-if(WA == 337 && selectAlturaPoste == '9'){
-    WB = 1977	
+if (WA == 337 && selectAlturaPoste == '9') {
+    WB = 1977
 };
-if(WA == 476 && selectAlturaPoste == '9'){
-    WB = 2308	
+if (WA == 476 && selectAlturaPoste == '9') {
+    WB = 2308
 };
-if(WA == 337 && selectAlturaPoste == '12'){
-    WB = 2929	
+if (WA == 337 && selectAlturaPoste == '12') {
+    WB = 2929
 };
-if(WA == 476 && selectAlturaPoste == '12'){
-    WB = 3329	
+if (WA == 476 && selectAlturaPoste == '12') {
+    WB = 3329
 };
-if(WA == 859 && selectAlturaPoste == '12'){
-    WB = 4471	
+if (WA == 859 && selectAlturaPoste == '12') {
+    WB = 4471
 };
-const MA = 0.9 * MB * WA/WB;
-const I = (3.14*(WB - WA))/64;
-const FLABAGEM = (3.14 * E * I)/(LF*LF);
+const MA = 0.9 * MB * WA / WB;
+const I = (3.14 * (WB - WA)) / 64;
+const FLABAGEM = ((3.14 * E * I) / (LF * LF));
 
-btnCalcular.onclick = () => {
-    
+function calcular() {
+    RES1.innerHTML =
+        `<div id="RES01">
+                <br>
+                <b>FORÇA DE TRAÇÃO NA LINHA PRIMÁRIA</b>
+                <p >${selectFP.value}</p>
+         </div>`;
+
+    RES2.innerHTML =
+        `<div id="RES02">
+            <b>FORÇA DE TRAÇÃO NA LINHA SECUNDÁRIA</b>
+                <p>${selectFS.value}</p>
+        </div>`;
+
+    RES3.innerHTML =
+        `<div id="RES03">
+            <br>
+            <b>FORÇA DO VENTO NA SUPERFÍCIE DO POSTE</b>
+            <p>${VB}</p>
+         </div>`;
+
+    RES4.innerHTML =
+        `<div id="RES04">
+            <br>
+            <b>FLAMBAGEM:</b>
+            <p>${FLABAGEM}</p>
+        </div>`;
 };
